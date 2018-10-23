a503295254325168a1f5fffee74ff11be7894fd3
djok0127 <djok0127@gmail.com>
2018-10-22 21:11:52 -0700

    Add files via upload

--- VGA/VGA.srcs/sources_1/new/top_module.v
+++ VGA/VGA.srcs/sources_1/new/top_module.v
@@ -1,127 +1,42 @@
 `timescale 1ns / 1ps
-/****************************** C E C S  3 6 0 ******************************
- * 
- * File Name:  vga.v
- *
- * Created by       Jordan Mark Gammad on 3/15/18.
- * Copyright © 2018 Jordan Mark Gammad. All rights reserved.
- *
- * In submitting this file for class work at CSULB
- * I am confirming that this is my work and the work
- * of no one else. In submitting this code I acknowledge that
- * plagiarism in student project work is subject to dismissal
- * from the class.
- *         
- ****************************************************************************/
-module vga(clk_100MHz, rst, sw, hsync, vsync, video, pixel_tick, pixel_x, pixel_y, rgb);
-    input        clk_100MHz, rst;
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/18/2018 03:25:41 PM
+// Design Name: 
+// Module Name: top_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module top_module(clk, reset, sw, color, h_sync, v_sync);
+    
+    input        clk, reset;
     input [11:0] sw;
-   output        hsync, vsync, video, pixel_tick;
-   output  [9:0] pixel_x, pixel_y;
-   output [11:0] rgb;
-     wire        clk_100MHz, rst, hsync, vsync, video, pixel_tick;
-     wire  [9:0] pixel_x, pixel_y;
-     wire [11:0] rgb;
+    wire       video_on;
+    wire [9:0] v_count, h_count;    
+    output h_sync, v_sync;
+    output [11:0] color;
      
-   // Constant Declaration
-   // VGA 640-by-480 sync parameters
-   localparam HD = 640, // Horizontal display area
-              HF = 48,  // Horizontal left
-              HB = 16,  // Horizontal right
-              HR = 96,  // Horizontal retrace
-              VD = 480, // Vertical display area
-              VF = 10,  // Vertical top
-              VB = 33,  // Vertical bottom
-              VR = 2;   // Vertical retrace
-              
-   // RGB
-   reg [11:0] rgb_reg;
-   // Sync counters
-   reg [9:0] h_count_reg, h_count_next;
-   reg [9:0] v_count_reg, v_count_next;
-   // Output buffer
-   reg       v_sync_reg, h_sync_reg;
-   reg       v_sync_next, h_sync_next;
-   // Status signal
-   wire      h_end, v_end, tick;
-   
-   // Body
-   // Registers
-   always @ (posedge clk_100MHz or posedge rst)
-      if (rst) 
-         begin
-            v_count_reg <= 0;
-            h_count_reg <= 0;
-            v_sync_reg <= 1'b0;
-            h_sync_reg <= 1'b0;
-            rgb_reg <= 12'b0;
-         end
-      else 
-         begin
-            v_count_reg <= v_count_next;
-            h_count_reg <= h_count_next;
-            v_sync_reg <= ~v_sync_next;
-            h_sync_reg <= ~h_sync_next;
-            rgb_reg <= sw;
-         end
-         
-   // Circuit to generate 25 MHz enable tick
-   reg    [1:0] count, n_count;
-   // Behavioral section for writing to registers   
-   always @ (posedge clk_100MHz or posedge rst)
-      if (rst) count <= 2'b0;
-         else count <= n_count;
-   // Wire output 
-   assign tick = (count == 3);
-   // Comparator
-   always @ (*)
-      n_count = (tick) ? 2'b0 : count + 2'b1;
-      
-   // Status signals
-   // End of horizontal counter (799)
-   assign h_end = (h_count_reg == (HD + HF + HB + HR - 1));
-   // End of vertical counter (524)
-   assign v_end = (v_count_reg == (VD + VF + VB + VR - 1));
-   
-   // Next-state logic of mod-800 horizontal sync counter
-   always @ (*)
-      if (tick)
-         if (h_end)
-            h_count_next = 0;
-         else
-            h_count_next = h_count_reg + 10'b1;
-      else
-         h_count_next = h_count_reg;   
- 
-   // Next-state logic of mod-525 vertical sync counter 
-   always @ (*)
-      if (tick & h_end)
-         if (v_end)
-            v_count_next = 0;
-         else
-            v_count_next = v_count_reg + 10'b1;
-      else
-         v_count_next = v_count_reg;
-         
-   // Horizontal and vertical sync, buffered to avoid glitch
-   // h_sync_next asserted between 656 and 751
-   always @ (*)
-      h_sync_next = (h_count_reg >= (HD + HB) && 
-                     h_count_reg <= (HD + HB + HR - 1));
-   // h_sync_next asserted between 490 and 491
-   always @ (*)
-      v_sync_next = (v_count_reg >= (VD + VF) && 
-                     v_count_reg <= (VD + VF + VR - 1));    
-      
-   // Video on/off
-   assign video = (h_count_reg < HD) && (v_count_reg < VD);
-   
-   // Output
-   assign hsync = h_sync_reg;
-   assign vsync = v_sync_reg;
-   assign pixel_x = h_count_reg;
-   assign pixel_y = v_count_reg;
-   assign pixel_tick = tick;
-   assign rgb = (video) ? rgb_reg : 12'b0;
-
+    vga_sync vs(.clk(clk), 
+                .reset(reset), 
+                .h_sync(h_sync), 
+                .v_sync(v_sync),
+                .video_on(video_on), 
+                .v_count(v_count),
+                .h_count(h_count)
+                );
+                
+	//assign the color of the screen based on switch inputs
+    assign color = (video_on) ? sw : 12'b0;
+                
 endmodule
--- VGA/VGA.srcs/sources_1/new/vga.v
+++ VGA/VGA.srcs/sources_1/new/vga.v
@@ -1,10 +1,12 @@
 `timescale 1ns / 1ps
 /****************************** C E C S  3 6 1 ******************************
  * 
- * File Name:  vga_sync
+ * File Name:  vga
  *
  * Created by       Dong Jae Shin 10/9/2018
  * Copyright © 2018 Dong Jae Shin. All rights reserved.
+ *
+ * purpose:
  *
  * In submitting this file for class work at CSULB
  * I am confirming that this is my work and the work
@@ -15,15 +17,17 @@
  ****************************************************************************/
 
 
-module vga_sync(clk, reset, h_sync, v_sync, video_on, h_count, v_count);
+module vga_sync(clk, reset, h_sync, v_sync, video_on, v_count, h_count );
+    
+    // initialization of input
+	input 		      clk, reset;
+    // initialization of output
+	output            video_on;
+    output reg [9:0]  v_count, h_count;	
+    output wire       h_sync, v_sync;
 
-	input 		 clk, reset;
-	output video_on;
-    output reg [9:0] v_count, h_count;	
-    output wire h_sync, v_sync;
-
-	reg [2:0] count, d;
-	reg [9:0] dv, dh;
+    // initialization of regs and wires
+	reg [1:0] count, d;
  	reg h_video_on, v_video_on;
 	wire tick, h_end, v_end;
 	
@@ -51,29 +55,25 @@ module vga_sync(clk, reset, h_sync, v_sync, video_on, h_count, v_count);
 	always @ (*) begin
 	   if(tick) begin
 	       if(h_end) h_count = 10'b0;
-	       else      dh = h_count + 10'b1;
-	       if(v_end) v_count = 10'b0;
-	       else      
-	           if(h_end)
-	               dv = v_count + 10'b1;
+	       else h_count = h_count + 10'b1;
+	       
+	       if(h_end&&v_end) v_count = 10'b0;
+	       else if(h_end) v_count = v_count + 10'b1;
 	   end
-	end
- 
+    end
     // behavioral blcok for v count and h count
     always @ (posedge clk or posedge reset) begin
         if(reset)begin
             v_count <= 10'b0;
             h_count <= 10'b0;
-            dh <= 10'b0;
-            dv <= 10'b0;
         end else begin
-            v_count <= dv;
-            h_count <= dh;
+            v_count <= v_count;
+            h_count <= h_count;
         end
     end
 
     // make 25 Mhz tick
-    assign tick = (count == 4);
+    assign tick = (count == 3);
     
     // check when the counter for horizontal and vertical ends
     assign h_end = (h_count == 799);
@@ -81,8 +81,10 @@ module vga_sync(clk, reset, h_sync, v_sync, video_on, h_count, v_count);
     
     // assgin h sync and v sync
     assign h_sync = ~(h_count >= 656 & h_count <= 751);
-    assign v_sync = ~(v_count >= 489 & v_count <= 491);
+    assign v_sync = ~(v_count >= 490 & v_count <= 491);
     
     // assert video_on when v count is less than 480 and h count is less than 640
     assign video_on = ((v_count < 480) & (h_count < 640));
+    
+
 endmodule
